46799df08bd167d8deaa13f66821993442666c6e
Ariel Vardy <77558607+ArJonVar@users.noreply.github.com>
2023-04-07 13:29:56 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -67,9 +67,6 @@ def ss_api_calls(scopeObjectId, logr=ghetto_logger('main.py', False)):
         error = "APIERROR: failure to find scopeObjectId, this means api key cannot see the sheet webhook is looking at"
         return(error)
 
-# trying to optimize by have this preloaded
-sheet_data = ss_api_calls(8025857521411972)
-
 @log_exceptions
 def row_id_to_row_dict(row_id, sheet, event_type, logr=ghetto_logger('main.py', False)):
     '''pulls data on webhook row id (url, row index) to make it clear what is happening before script runs'''
@@ -96,8 +93,6 @@ async def plupdate(payload: WebhookPayload):
     logr=ghetto_logger("main.py")
     webhook_id = payload.webhookId
     scopeObjectId=payload.scopeObjectId
-    if str(scopeObjectId) != str(8025857521411972):
-        sheet_data = ss_api_calls(scopeObjectId)
     logr.log(str(payload))
 
     # Extract the events into a list of dictionaries
@@ -107,13 +102,14 @@ async def plupdate(payload: WebhookPayload):
         events.append(event_dict)
     
     logr.log(str(events))
-    logr.log(str(type(sheet_data)))
+
 
     if str(webhook_id) == '7589161210275716':
         '''if the webhook_id is a match, pull ss data from sheet, and then use webhook payload and ss to extract meaningful data (url/row index)
             so the logging can easily help a human see which row of which sheet is triggering'''
         data = []
         rows= []
+        sheet_data = ss_api_calls(scopeObjectId)
         for event in events:
             event_type = event.get('eventType')
             row_id = event.get('rowId')
